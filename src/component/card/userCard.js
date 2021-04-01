9b32a5236cf6cb67fe4458168c6478933e18781c
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-04-01 18:18:17 +0300

    Added lise-active icon

--- src/component/card/userCard.js
+++ src/component/card/userCard.js
@@ -1,46 +1,61 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import { removeUser, setModal } from "../../actions";
 
-import * as Icons from '../icons';
-import styles from './userCard.module.css'
+import * as Icons from "../icons";
+import styles from "./userCard.module.css";
 
 function UserCard(props) {
-  return (
-    <div className={styles.Card}>
-      <div className={styles.ImgCover}>
-        <img src={`https://avatars.dicebear.com/v2/avataaars/${props.user.username}.svg?options[mood][]=happy`} alt="Avatar"/>
-      </div>
+	const [isLiked, setIsLiked] = useState(false);
 
-      <div className={styles.CardBody}>
-        <h3>{props.user.name}</h3>
-        <span>
-          <Icons.Mail /> 
-          <p>{props.user.email}</p>
-        </span>
-        <span>
-          <Icons.Phone /> 
-          <p>{props.user.phone}</p>
-        </span>
-        <span>
-          <Icons.Web /> 
-          <p>http://{props.user.website}</p>
-        </span>
-      </div>
+	return (
+		<div className={styles.Card}>
+			<div className={styles.ImgCover}>
+				<img
+					src={`https://avatars.dicebear.com/v2/avataaars/${props.user.username}.svg?options[mood][]=happy`}
+					alt="Avatar"
+				/>
+			</div>
 
-      <div className={styles.CardFooter}>
-        <button className={styles.Like}><Icons.Like /></button>
-        <button onClick={() => props.setModal([true, props.user.id])}><Icons.Edit /></button>
-        <button onClick={() => props.removeUser(props.user.id)}><Icons.Delete /></button>
-      </div>
-    </div>
-  );
+			<div className={styles.CardBody}>
+				<h3>{props.user.name}</h3>
+				<span>
+					<Icons.Mail />
+					<p>{props.user.email}</p>
+				</span>
+				<span>
+					<Icons.Phone />
+					<p>{props.user.phone}</p>
+				</span>
+				<span>
+					<Icons.Web />
+					<p>http://{props.user.website}</p>
+				</span>
+			</div>
+
+			<div className={styles.CardFooter}>
+				<button
+					className={styles.Like}
+					onClick={() => setIsLiked(!isLiked)}
+				>
+					{isLiked ? <Icons.Like /> : <Icons.LikeActive />}
+				</button>
+				<button onClick={() => props.setModal([true, props.user.id])}>
+					<Icons.Edit />
+				</button>
+				<button onClick={() => props.removeUser(props.user.id)}>
+					<Icons.Delete />
+				</button>
+			</div>
+		</div>
+	);
 }
 
 const mapStateToProps = (state) => {
-  return {
-    userData: state.userData,
-    isModalOpen: state.isModalOpen
-  };
+	return {
+		userData: state.userData,
+		isModalOpen: state.isModalOpen,
+	};
 };
 
 export default connect(mapStateToProps, { removeUser, setModal })(UserCard);
